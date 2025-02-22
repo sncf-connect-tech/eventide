0488e82c77ea2f2ba0830ecd82a2fac3a8ac8a92
Alexis Choupault <alexis_choupault@connect-tech.sncf>
2025-02-22 14:56:08 +0100

    expose ETAccount

--- lib/eventide.dart
+++ lib/eventide.dart
@@ -1,7 +1,7 @@
 library eventide;
 
 export 'src/eventide.dart' show Eventide;
-export 'src/eventide_platform_interface.dart' show ETCalendar, ETEvent;
+export 'src/eventide_platform_interface.dart' show ETCalendar, ETEvent, ETAccount;
 export 'src/eventide_exception.dart'
     show
         ETException,
